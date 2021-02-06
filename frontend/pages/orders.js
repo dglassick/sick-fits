46d2808bab19765baabdfe2c3eb8feabb855d50a
Derek Glassick <derek.glassick@gmail.com>
2021-02-06 11:51:18 -0800

    started making header and nav for site, still getting used to Next.js documentation

---
+++ frontend/pages/orders.js
@@ -0,0 +1,3 @@
+export default function OrderPage() {
+  return <p>hello, I'm the order page</p>;
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,3 @@
+export default function SellPage() {
+  return <p>hello, I am the sell page</p>;
+}
