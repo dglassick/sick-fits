501699f82d7ec8a78119f0156276ad4e5903cb1f
Derek Glassick <derek.glassick@gmail.com>
2021-02-11 08:24:13 -0800

    using TS files on the backend, helps the connection with keystone and the frontend

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,12 +1,13 @@
 import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
-import { Product } from './schemas/Product';
-import 'dotenv/config';
 import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -37,12 +38,18 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: add data seeding here
+      async onConnect(keystone) {
+        console.log('connected to the database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // show the UI only for people who pass this test
