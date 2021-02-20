d624745167305309e46416c771ec3912a46059ea
Derek Glassick <derek.glassick@gmail.com>
2021-02-20 14:06:59 -0800

    finished form and connected it to the backend, made a single product component that is automatically routed to when the user submits a product.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,35 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 import Form from './styles/Form';
+import Router from 'next/router';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    #   which variables get passed in and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
@@ -9,14 +39,29 @@ export default function CreateProduct() {
     description: 'Nice shoes'
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }]
+    }
+  );
   return (
     <Form
-      onSubmit={e => {
+      onSubmit={async e => {
         e.preventDefault();
         console.log(inputs);
+        // submit the input fields to the backend
+        const res = await createProduct();
+        clearForm();
+        // go to that product's page!
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`
+        });
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor='image'>
           Image
           <input
